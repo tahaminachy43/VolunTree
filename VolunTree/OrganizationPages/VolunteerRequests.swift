ff84cc3cb6ce7bb540bd6ba5f069e8c77bec31e5
Khushi <khushichoksi1@gmail.com>
2025-03-19 11:22:28 -0600

    Volunteer Requests and Current Volunteers screens. Not fully done but has most functionality.

--- VolunTree/OrganizationPages/VolunteerRequests.swift
+++ VolunTree/OrganizationPages/VolunteerRequests.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 struct VolunteerRequests: View {
+    @State private var requests: [(position: String, name: String)] = [
+        ("Recreational Assistant", "Alice Johnson"),
+        ("Student Volunteer", "Bob Smith"),
+        ("Helper", "June Lindt"),
+        ("Social Worker", "Linda Brown"),
+        ("Social Worker", "Alice Johnson"),
+        ("Student Volunteer", "Bob Smith"),
+        ("Recreational Assistant", "Alice Smith")
+        ] // sample data
+    
+    @State private var navigateToProfile = false
+    @State private var selectedIndex: Int?
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -21,16 +34,48 @@ struct VolunteerRequests: View {
                         .padding()
                         .foregroundStyle(Color.darkGreen)
                     
-                    Spacer()
-                    
-                    Text("test")
+                    // request cards
+                    ScrollView {
+                        LazyVStack(spacing: 110) {
+                            ForEach(requests.indices, id: \.self) { index in
+                                VolunteerCard(
+                                    position: requests[index].position,
+                                    name: requests[index].name,
+                                    onButtonTap: {
+                                        navigateToProfile = true
+                                        selectedIndex = index
+                                    }
+                                )
+                                .padding(.horizontal)
+                                
+                                // Delete button
+    
+                            }
+                        }
+                        .padding(.top, 50)
+                        .padding(.bottom, 100)
+                        .background(
+                            NavigationLink(
+                                destination: ApplicationPage(position: requests[selectedIndex ?? 0].position, name: requests[selectedIndex ?? 0].name),
+                                isActive: $navigateToProfile,       // this is deprecated, need to find another way
+                                label: { EmptyView() }
+                            )
+                            .foregroundStyle(Color.darkGreen)
+                            .background(Color.darkGreen)
+                            .navigationBarBackButtonHidden(true)
+                        )
+                        
+                    }
+
                                 
                 }
                 .frame(maxHeight: .infinity, alignment: .top)
+                
             }
         }
        
     }
+    
 }
 
 #Preview {
