b42d991a652bf11d34ea9443ed8e73b5dff5a7fe
Code_with_Moon <tahaminachy43@gmail.com>
2025-04-09 14:23:54 -0600

    Update register so that its responsive

--- VolunTree/Register.swift
+++ VolunTree/Register.swift
@@ -22,115 +22,120 @@ struct Register: View {
     
     var body: some View {
         NavigationStack {
-            GeometryReader { geometry in
+            
                 ZStack {
                     Image("background")
                         .resizable()
                         .scaledToFill()
                         .edgesIgnoringSafeArea(.all)
-                    
-                    VStack(spacing: 0) {
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text("Voluntree")
-                                .font(.custom("Pacifico", size: 47))
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                            
-                            Text("All Your")
-                                .font(.title)
-                                .foregroundColor(.white)
-                            
-                            Text("volunteering")
-                                .font(.title)
-                                .foregroundColor(.white)
-                            
-                            Text("needs in one place")
-                                .font(.title)
-                                .foregroundColor(.white)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                        .padding(.bottom, 20)
+                    ScrollView{
                         
-                        VStack(spacing: 20) {
-                            Text("Create an account")
-                                .font(.title2)
-                                .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .padding(.top, 20)
-                            
-                            TextField("Full Name", text: $fullName)
-                                .padding()
-                                .background(Color.white.opacity(0.8))
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                            
-                            TextField("Email", text: $email)
-                                .padding()
-                                .background(Color.white.opacity(0.8))
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                            
-                            SecureField("Password", text: $password)
-                                .padding()
-                                .background(Color.white.opacity(0.8))
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                            
-                            SecureField("Confirm Password", text: $confirmPassword)
-                                .padding()
-                                .background(Color.white.opacity(0.8))
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                            
-                            //                          If there is an error
-                            if let errorMessage = errorMessage {
-                                Text(errorMessage)
-                                    .foregroundColor(.red)
-                                    .padding(.horizontal)
-                            }
-                            
-                            Button(action: registerNewUser) {
-                                if isLoading{
-                                    ProgressView()
-                                }else{
-                                    Text("Register")
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                        .frame(maxWidth: .infinity)
-                                        .padding()
-                                        .background(Color.darkColor)
-                                        .cornerRadius(10)
-                                }
+                        
+                        
+                        VStack(spacing: 0) {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Voluntree")
+                                    .font(.custom("Pacifico", size: 47))
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
                                 
+                                Text("All Your")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                
+                                Text("volunteering")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                
+                                Text("needs in one place")
+                                    .font(.title)
+                                    .foregroundColor(.white)
                             }
-                            .disabled(isLoading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                             .padding(.horizontal)
                             .padding(.bottom, 20)
                             
-                            HStack {
-                                Text("Already Have an Account?")
+                            VStack(spacing: 20) {
+                                Text("Create an account")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
                                     .foregroundColor(.black)
-                                NavigationLink(destination: SignIn()) {
-                                    Text("Sign In")
-                                        .foregroundColor(Color.darkColor)
-                                        .fontWeight(.bold)
+                                    .padding(.top, 20)
+                                
+                                TextField("Full Name", text: $fullName)
+                                    .padding()
+                                    .background(Color.white.opacity(0.8))
+                                    .cornerRadius(10)
+                                    .padding(.horizontal)
+                                
+                                TextField("Email", text: $email)
+                                    .padding()
+                                    .background(Color.white.opacity(0.8))
+                                    .cornerRadius(10)
+                                    .padding(.horizontal)
+                                
+                                SecureField("Password", text: $password)
+                                    .padding()
+                                    .background(Color.white.opacity(0.8))
+                                    .cornerRadius(10)
+                                    .padding(.horizontal)
+                                
+                                SecureField("Confirm Password", text: $confirmPassword)
+                                    .padding()
+                                    .background(Color.white.opacity(0.8))
+                                    .cornerRadius(10)
+                                    .padding(.horizontal)
+                                
+                                //                          If there is an error
+                                if let errorMessage = errorMessage {
+                                    Text(errorMessage)
+                                        .foregroundColor(.red)
+                                        .padding(.horizontal)
+                                }
+                                
+                                Button(action: registerNewUser) {
+                                    if isLoading{
+                                        ProgressView()
+                                    }else{
+                                        Text("Register")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                            .frame(maxWidth: .infinity)
+                                            .padding()
+                                            .background(Color.darkColor)
+                                            .cornerRadius(10)
+                                    }
+                                    
+                                }
+                                .disabled(isLoading)
+                                .padding(.horizontal)
+                                .padding(.bottom, 20)
+                                
+                                HStack {
+                                    Text("Already Have an Account?")
+                                        .foregroundColor(.black)
+                                    NavigationLink(destination: SignIn()) {
+                                        Text("Sign In")
+                                            .foregroundColor(Color.darkColor)
+                                            .fontWeight(.bold)
+                                    }
                                 }
                             }
+                            .padding()
+                            .background(Color.white.opacity(0.5))
+                            .cornerRadius(20)
+                            .shadow(radius: 10)
+                            .padding(.horizontal)
+                            
+                            NavigationLink("", destination: UserPreferences(userId: userId, userEmail: email, userPassword: password), isActive: $navigateToPreferences)
+                                .hidden()
                         }
-                        .padding()
-                        .background(Color.white.opacity(0.5))
-                        .cornerRadius(20)
-                        .shadow(radius: 10)
-                        .padding(.horizontal)
-                        
-                        NavigationLink("", destination: UserPreferences(userId: userId, userEmail: email, userPassword: password), isActive: $navigateToPreferences)
-                            .hidden()
+                        .padding(.top, 50)
                     }
                 }
             }
-        }
+        
     }
     
     func registerNewUser() {
