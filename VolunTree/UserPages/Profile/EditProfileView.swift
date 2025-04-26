3de1cedc8941b8e4e8d8c381a03fb5318d1eaa9b
Khushi <khushichoksi1@gmail.com>
2025-04-25 19:32:02 -0600

    Dummy view for edit profile page.

--- VolunTree/UserPages/Profile/EditProfileView.swift
+++ VolunTree/UserPages/Profile/EditProfileView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
+/// The edit profile view. The user can edit their name and bio. It is not fully completed yet.
 struct EditProfileView: View {
         
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
-            
+                Text("Edit Profile")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .foregroundStyle(Color.darkGreen)
+                    .multilineTextAlignment(.leading)
+                
+                Text("Coming soon...")
+                    .foregroundStyle(Color.darkGreen)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
             .background(Color.background)
+            .padding(20)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.background)
